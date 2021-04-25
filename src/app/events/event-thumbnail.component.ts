2ff762e5969649cfd5957c8e954cfed760833ff2
dino <mypascal2000@gmail.com>
2021-04-25 13:10:00 -0700

    Initial push

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input} from '@angular/core'
+
+@Component({
+  selector: 'event-thumbnail',
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{event.name}}</h2>
+      <div>Date: {{event.date}}</div>
+      <div>Time: {{event.time}}</div>
+      <div>Price: \${{event.price}}</div>
+      <div>
+        <span>Location: {{event.location.address}}</span>
+        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+      </div>
+    </div>
+    `,
+    styles:[`
+      .pad-left {margin-left: 10px;}
+      .well div {color: #bbb;}
+    `]
+})
+export class EventThumbnailComponent {
+  //@Input() decorator tells angular that this 'event' will be
+  //passed in from another component.  This is used to communicate
+  //to child components (this component) from parent component
+  //which is event-list component.
+  @Input() event:any
+
+}
