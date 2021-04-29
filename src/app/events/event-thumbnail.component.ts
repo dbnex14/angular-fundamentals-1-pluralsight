f049c0c68eb8a4499c0a904a307852c1e34ab2a0
dino <mypascal2000@gmail.com>
2021-04-29 06:37:14 -0700

    Added logic to hide and style using ngClass and ngStyle.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,14 @@ import {Component, Input} from '@angular/core'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
+      <!--<div [ngClass]="getStartTimeClass()"  apply style using ngClass or ngStyle -->
+      <div [ngStyle]="getStartTimeStyle()"
+           [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{event?.price}}</div>
       <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
@@ -18,6 +25,8 @@ import {Component, Input} from '@angular/core'
     </div>
     `,
     styles:[`
+      .green { color: #003300 !important; }
+      .bold { font-weight: bold; }
       .thumbnail {min-height:210px;}
       .pad-left {margin-left: 10px;}
       .well div {color: #bbb;}
@@ -30,4 +39,25 @@ export class EventThumbnailComponent {
   //which is event-list component.
   @Input() event:any
 
+  getStartTimeClass() {
+    // ngClass can return (a) object, (b) space separated class names, or (c) array of classes
+    // const isEarlyStart = this.event && this.event.time ==='8:00 am'
+    // return {green: isEarlyStart, bold: isEarlyStart} // to return an object
+
+    // but ngClass also allows returning string of space separated class names we want to return
+    // if (this.event && this.event.time === '8:00 am')
+    //   return 'green bold'
+    // return '';
+
+    // or return array of classes
+    if (this.event && this.event.time === '8:00 am')
+      return ['green', 'bold']
+    return [];
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00 am')
+      return {color: '#003300', 'font-weight': 'bold'}
+    return {}
+  }
 }
